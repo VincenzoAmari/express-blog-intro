cd57a0c8f5f0654ed9b487f919b16a1258c4a6b9
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-10 14:46:22 +0100

    app.get

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get();
+app.get("/img/:imageName", (req, res) => {
+  res.send(`<img src="/img/${req.imageName}" alt="Immagine">`);
+});
 
 // Array di post
 const posts = [
